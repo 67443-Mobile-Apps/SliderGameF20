bec5da378d45230c55fcf05dbfc504ab22e88dc1
profh <profh@cmu.edu>
2020-09-07 09:14:09 -0400

    Initial Commit

---
+++ SliderGameF20/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SliderGameF20
+//
+//  Created by Larry Heimann on 9/7/20.
+//  Copyright © 2020 Larry Heimann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
